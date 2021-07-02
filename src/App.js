1c77ccf4b43cf59c946ec2fee70f271b3ae05990
Skyler Vassallo <Skyler_Vassallo@glic.com>
2021-07-02 17:12:51 -0400

    Added About page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 import { Route, Link, withRouter } from 'react-router-dom';
 import axios from 'axios';
+
+import Bands from './Components/Bands/Bands';
 
 class App extends Component {
   constructor(props) {
@@ -22,11 +24,17 @@ class App extends Component {
               <li><Link to='/'>Home</Link></li>
               <li><Link to='/signup'>Signup</Link></li>
               <li><Link to='/login'>Log In</Link></li>
+              <li><Link to='/about'>About</Link></li>
             </ul>
           </nav>
         </header>
         <main>
           <p>Welcome to Bandspace...2~!</p>
+          <Route path="/bands" render={() =>
+            <Bands
+              band_name ={this.state.band_name}
+            />
+          } />
         </main>
       </div>
     );
---
+++ src/Components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <p>This is going to be the about page for Bandspace.</p>
+        </div>
+    )
+}
+
+export default About;
